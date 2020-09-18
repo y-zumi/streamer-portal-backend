e60e68e2a79fb0c9cef42fe15e138d080a727947
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-09-18 16:25:49 +0900

    add: serve live_statuses endpoint using dummy data

--- main.go
+++ main.go
@@ -1,25 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type GetLiveStatusesRequest struct {
+	StreamerID string `json:"streamer_id"`
+}
+
+type GetLiveStatusesResponse struct {
+	LiveStatuses []LiveStatus `json:"live_statuses"`
+}
+
+type LiveStatus struct {
+	PlatformType string `json:"platform_type"`
+	IsLive       bool   `json:"is_live"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("helloworld: received a request")
-	target := os.Getenv("TARGET")
-	if target == "" {
-		target = "World"
+	var req GetLiveStatusesRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		fmt.Printf("decode error: %v", err)
+		return
 	}
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	fmt.Printf("req: %v", req)
+
+	resp := GetLiveStatusesResponse{
+		LiveStatuses: []LiveStatus{
+			{
+				PlatformType: "youtube",
+				IsLive:       true,
+			},
+			{
+				PlatformType: "twitch",
+				IsLive:       false,
+			},
+			{
+				PlatformType: "niconico",
+				IsLive:       false,
+			},
+		},
+	}
+
+	b, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Printf("failed to marshal resp: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	if _, err := w.Write(b); err != nil {
+		fmt.Printf("failed to write resp: %v", err)
+		return
+	}
 }
 
 func main() {
 	log.Print("helloworld: starting server...")
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/live_statuses", handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
